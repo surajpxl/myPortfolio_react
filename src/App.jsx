4f11c0cc84f48eced3af9c4e3b027e2376c24ffb
surajpxl <guptasuraj4455@gmail.com>
2025-10-05 23:11:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 // App.jsx
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 // import shake from './styles/shake.css'; // Assuming you have a shake.css file for animations
 
 function App() {
   return (
     <Router>
-      <ScrollToTop/>
+      <ScrollToTop />
       <Navbar />
       <Home />
       <About />
